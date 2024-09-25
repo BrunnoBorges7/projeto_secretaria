3d9d24c180e1ecfe4a369a02cd3461c8950707dd
BrunnoBorges7 <brunno.borgesp17@gmail.com>
2024-09-25 12:34:29 -0300

    Configuração dos Templates e inicio da Home Page

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,5 +1,11 @@
+from flask import render_template           
 from app import app
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/home/<user>")
+@app.route("/", defaults={"user" :None})
+def index(user):
+
+    return render_template('index.html' , 
+                            user=user)
+
+
