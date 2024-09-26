a945138f23fceb92cafb0b0c197d68d3b2d8d8de
BrunnoBorges7 <brunno.borgesp17@gmail.com>
2024-09-26 14:12:05 -0300

    Configuração dos templates da Home Page e Base HTML

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,11 +1,7 @@
-from flask import render_template           
+from flask import render_template
 from app import app
 
-@app.route("/home/<user>")
-@app.route("/", defaults={"user" :None})
+@app.route("/index/<user>")
+@app.route("/", defaults={"user": None})
 def index(user):
-
-    return render_template('index.html' , 
-                            user=user)
-
-
+    return render_template('index', user=user)
